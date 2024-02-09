aa92ca3e4ed758e5bd4ef254553060f6453e080f
leandro <leandro.agustin20@outlook.com>
2024-02-09 13:35:38 -0300

    segundo commit

---
+++ proyects/00-hola-mundo/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { TwitterCard } from './twitterCard'
+
+export default function App() {
+  return (
+    <>
+      <section className='App'>
+        <TwitterCard name="Miguel Angel Durán" username=""/>
+      </section>
+    </>
+  )
+}
+
---
+++ proyects/00-hola-mundo/src/TwitterCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export const TwitterCard = ({name, username, isFollowing}) => {
+    const imageSrc = `https://unavatar.io/${username}`
+  return (
+    <>
+        <article className='tw-followCard'>
+            <header className='tw-followCard-header'>
+                <img className='tw-followCard-avatar' src={imageSrc} alt="el avatar de midudev" />
+                <div className='tw-followCard-info'>
+                    <strong>{name}</strong>
+                    <span className='tw-followCard-infoUsername'>{username}</span>
+                </div>
+            </header>
+            <aside>
+                <button className='tw-followCard-button'>Seguir</button>
+            </aside>
+        </article>
+    </>
+  )
+}
